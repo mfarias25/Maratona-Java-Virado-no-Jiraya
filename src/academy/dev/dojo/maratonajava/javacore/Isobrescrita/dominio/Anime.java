0c776d380c81652204763406101206adc2d602a6
Mateus Farias <mateusfariasdev@outlook.com>
2023-09-23 19:32:42 -0300

    toString

---
+++ src/academy/dev/dojo/maratonajava/javacore/Isobrescrita/dominio/Anime.java
@@ -0,0 +1,24 @@
+package academy.dev.dojo.maratonajava.javacore.Isobrescrita.dominio;
+
+public class Anime { //extends Object
+    private String nome;
+
+    @Override
+    public String toString() {
+        return "Anime{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/academy/dev/dojo/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.dev.dojo.maratonajava.javacore.Isobrescrita.test;
+
+
+import academy.dev.dojo.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+        System.out.println(anime);
+    }
+}
