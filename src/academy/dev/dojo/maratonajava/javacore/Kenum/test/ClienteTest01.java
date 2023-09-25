21324083060b34bee045916aa664660c67a60f4e
Mateus Farias <mateusfariasdev@outlook.com>
2023-09-25 16:35:18 -0300

    enumeração - construtores&atributos

--- src/academy/dev/dojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/dev/dojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -1,18 +1,14 @@
 package academy.dev.dojo.maratonajava.javacore.Kenum.test;
 
 import academy.dev.dojo.maratonajava.javacore.Kenum.dominio.Cliente;
+import academy.dev.dojo.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
 import academy.dev.dojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Jiraya", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Jaspion", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Mikey", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente4 = new Cliente("Pato Donald", TipoCliente.PESSOA_FISICA);
+        Cliente cliente1 = new Cliente("Jiraya", TipoCliente.PESSOA_FISICA, TipoPagamento.Debito);
+        Cliente cliente2 = new Cliente("Jaspion", TipoCliente.PESSOA_JURIDICA, TipoPagamento.Credito);
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
-
     }
 }
